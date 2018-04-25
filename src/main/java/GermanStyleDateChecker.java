43cbeaf57af6192258285b6e44b785f3e8c46129
hannesgerstmayr <hannes.gerstmayr@gmail.com>
2018-04-25 16:53:48 +0200

    \\d instead of [0-9]

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -14,8 +14,9 @@ import java.util.regex.Pattern;
 public class GermanStyleDateChecker {
 
     public static Matcher getMatcher(String dateString) {
-        Pattern p = Pattern.compile("^([0-9]|[0-2][0-9]|3[0-1])(\\.|-| )([0-9]|0[0-9]|1[0-2])(\\.|-| )([0-9][0-9]|19[0-9][0-9]|20[0-9][0-9])?$"); //statt " " auch \\s möglich
+        Pattern p = Pattern.compile("^(\\d|[0-2]\\d|3[0-1])(\\.|-| )(\\d|0\\d|1[0-2])(\\.|-| )(\\d\\d|19\\d\\d|20\\d\\d)?$"); //statt " " auch \\s möglich
 
+        //
         //(www\\.)?     optional www. zu beginn
         //(\\w)+        >=1 beliebige Zeichen
         //(-(\\w)+)*    0 oder mehrere Teile mit Bindestrichen
